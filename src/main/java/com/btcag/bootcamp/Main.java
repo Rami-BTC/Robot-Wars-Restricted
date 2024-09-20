2a45b2693717629f5c3e02db965d4a663a4f1b3e
Abdullatif <rami.abdullatif@btc-ag.com>
2024-09-20 13:56:11 +0200

    Spielfeld mit Spieleranzeige... Ich kann nicht mehr...

--- src/main/java/com/btcag/bootcamp/Main.java
+++ src/main/java/com/btcag/bootcamp/Main.java
@@ -8,6 +8,7 @@ public class Main {
         String username = "";
         String roboName = "";
         int roboNumber = 0;
+        boolean robotShown = false;
         String input;
 
         System.out.println("""
@@ -74,11 +75,94 @@ public class Main {
         System.out.println("""
                       Sehr schön, jetzt brauchen wir nur noch einen Namen...:
                 """);
+
         //-----------------------------------
         Scanner sc2 = new Scanner(System.in);
         roboName = sc2.nextLine();
+        //-----------------------------------
 
-        System.out.println(roboNumber);
 
+        int length = 15;
+        int height = 10;
+
+        int currentPosLength = 0;
+        int currentPosHeight = 0;
+
+        boolean gameOn = true;
+        int movementDir = 0;
+
+        while (gameOn) {
+            System.out.println("""
+                \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n
+                ~~~~~~~~~~~~//~~~~~~~~~~~~~~~~~~~||~~~~~~~~~~~~~~~~~~~//~~~~~~~~~~~~
+                """);
+
+            System.out.println("Dein Name: " + username + "\nDein Robotername: " + roboName);
+            int tmp = roboNumber;
+            while (!robotShown) {
+                while (tmp == 1) {
+                    System.out.println("""
+                             _._      _,-'" "`-._
+                            (,-.` ._,'(        |\\`-/|
+                                ` -.-' \\ )- ` ( , o o)
+                                       `-     \\`_`"'-
+                            """);
+                    tmp = 0;
+                }
+
+                while (tmp == 2) {
+                    System.out.println("""
+                            ("`-''-/").___..--''"`-._\s
+                             `6_ 6  )   `-.  (     ).`-.__.`)\s
+                             (_Y_.)'  ._   )  `._ `. ``-..-'\s
+                               _..`--'_..-_/  /--'_.'
+                              ((((.-''  ((((.'  (((.-'\s
+                            """);
+                    tmp = 0;
+                }
+                robotShown = true;
+                System.out.println();
+            }
+            robotShown = false;
+
+            System.out.println("Spielfeld: ");
+            int currentHeight = 0;
+            while (currentHeight < height) {
+                int currentLength = 1;
+                while (currentLength < length) {
+
+                    int tmp2 = currentLength;
+                    int tmp4 = -1;
+                    while (tmp2 == currentPosLength){
+                        int tmp3 = currentHeight;
+                        while (tmp3 == currentPosHeight){
+                            System.out.print("["+roboName.charAt(0)+"]");
+                            tmp4 = tmp2;
+                            tmp2--;
+                            tmp3--;
+                        }
+                        tmp2 = -1;
+                    }
+                    while (currentLength != tmp4){
+                        System.out.print("[ ]");
+                        tmp4 = currentLength;
+                    }
+                    currentLength++;
+                }
+                System.out.println();
+                currentHeight++;
+            }
+
+            System.out.println("""
+                   
+                    Nach Links: [1]
+                    Nach Rechts: [2]
+                    Nach Oben: [3]
+                    Nach Unten: = [4]
+                   
+                   """);
+
+            movementDir = sc.nextInt();
+        }
     }
 }
