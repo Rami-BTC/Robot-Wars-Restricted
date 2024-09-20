6bd9c36241052ad97ad32880fd4171e63f47faf5
Abdullatif <rami.abdullatif@btc-ag.com>
2024-09-20 12:57:48 +0200

    Willkommen & Roboter Auswahl mit Namensgebung

--- src/main/java/com/btcag/bootcamp/Main.java
+++ src/main/java/com/btcag/bootcamp/Main.java
@@ -6,10 +6,79 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String username = "";
+        String roboName = "";
+        int roboNumber = 0;
+        String input;
 
-        System.out.println("Willkommen bei RoboWars. Wie lautet dein Name? [Name Eingeben]: ");
+        System.out.println("""
+                       \s
+                            ⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀
+                            ⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀
+                            ⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀
+                            ⠀⠀⠀⠀⠀⣾⣿⣿⣿⣿⣿⣿⣿⡀⠀⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀
+                            ⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀
+                            ⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣠⣤⣤⣼⣿⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀
+                            ⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀
+                            ⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀
+                            ⠀⠀⠀⠘⣿⣿⣿⣿⠟⠁⠀⠀⠀⠹⣿⣿⣿⣿⣿⠟⠁⠀⠀⠹⣿⣿⡿⠀⠀⠀⠀⠀
+                            ⠀⠀⠀⠀⣿⣿⣿⡇⠀⠀⠀⢼⣿⠀⢿⣿⣿⣿⣿⠀⣾⣷⠀⠀⢿⣿⣷⠀⠀⠀⠀⠀
+                            ⠀⠀⠀⢠⣿⣿⣿⣷⡀⠀⠀⠈⠋⢀⣿⣿⣿⣿⣿⡀⠙⠋⠀⢀⣾⣿⣿⠀⠀⠀⠀⠀
+                            ⢀⣀⣀⣀⣿⣿⣿⣿⣿⣶⣶⣶⣶⣿⣿⣿⣿⣾⣿⣷⣦⣤⣴⣿⣿⣿⣿⣤⠤⢤⣤⡄
+                            ⠈⠉⠉⢉⣙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⣀⣀⣀⡀⠀
+               \s
+                Willkommen bei RoboWars. Wie lautet dein Name? [Name Eingeben]:\s""");
         username = sc.nextLine();
 
-        System.out.println("Ah, du bist es " + username + ", schön das du da bist!");
+        System.out.println("""
+                ~~~~~~~~~~~~//~~~~~~~~~~~~~~~~~~~||~~~~~~~~~~~~~~~~~~~//~~~~~~~~~~~~
+                """);
+        System.out.println(
+                "       Ah, du bist es " + username + ", schön, dass du da bist!\n");
+
+        System.out.println("""
+                                Drücke Enter um fortzufahren...
+                """);
+
+        //-----------------------------------
+        input = sc.nextLine();
+        //-----------------------------------
+
+        System.out.println("""
+                ~~~~~~~~~~~~//~~~~~~~~~~~~~~~~~~~||~~~~~~~~~~~~~~~~~~~//~~~~~~~~~~~~
+                """);
+
+        System.out.println("Wähle einen Katzenroboter aus!\n");
+        System.out.println("""
+                     _._      _,-'"" `- ._              ("`-''-/").___..--''"`-._\s
+                    (,-. `._ ,'(         |\\`-/|         `6_ 6  )   `-.  (     ).`-.__.`)\s
+                         `-. -' \\ )-`  (  o o)          (_Y_.)'  ._   )  `._ `. ``-..-'\s
+                               `-    \\`_ `"'-             _..`--'_..-_/  /--'_.'
+                                  --     --                ((((.-''  ((((.'  (((.-'\s
+                                   [ 1 ]                            [ 2 ]
+                """);
+
+        //-----------------------------------
+
+        while (roboNumber < 1){
+            roboNumber = sc.nextInt();
+            while (roboNumber > 2){
+                roboNumber = 0;
+            }
+        }
+
+        //-----------------------------------
+        System.out.println("""
+                ~~~~~~~~~~~~//~~~~~~~~~~~~~~~~~~~||~~~~~~~~~~~~~~~~~~~//~~~~~~~~~~~~
+                """);
+
+        System.out.println("""
+                      Sehr schön, jetzt brauchen wir nur noch einen Namen...:
+                """);
+        //-----------------------------------
+        Scanner sc2 = new Scanner(System.in);
+        roboName = sc2.nextLine();
+
+        System.out.println(roboNumber);
+
     }
 }
