d7f4389a8e8478bd3a50d6cef4e2fa308985634f
Abdullatif <rami.abdullatif@btc-ag.com>
2024-09-26 13:55:27 +0200

    'If & Else, Methoden und Arrays' wurden hinzugefügt!

--- src/main/java/com/btcag/bootcamp/Main.java
+++ src/main/java/com/btcag/bootcamp/Main.java
@@ -3,14 +3,140 @@ package com.btcag.bootcamp;
 import java.util.Scanner;
 
 public class Main {
+    public static String username;
+    public static int robotNumber = 0;
+    public static String robotName;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String username = "";
-        String roboName = "";
-        int roboNumber = 0;
-        boolean robotShown = false;
-        String input;
 
+        int[][] playArea = new int [10][15];
+        int currPosY = 0;
+        int currPosX = 0;
+
+        welcomeScreen(sc);
+        robotSelection(sc);
+        showStats();
+        generateField(playArea, currPosY, currPosX);
+    }
+
+    private static void generateField(int[][] playArea, int currPosY, int currPosX) {
+        for (int i = 0; i < playArea.length; i++){
+            for (int j = 0; j < playArea[i].length; j++){
+                System.out.print("[");
+                if ((i + 1) == currPosY && (j + 1)== currPosX){
+                    System.out.print(username.charAt(0));
+                } else {
+                    System.out.print(" ");
+                }
+                System.out.print("]");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void showStats(){
+        consoleClear();
+        System.out.println("Dein Name: " + username + "\nDein Robotername: " + robotName);
+        if (robotNumber == 0){
+            System.out.println("""
+                             _._      _,-'" "`-._
+                            (,-.` ._,'(        |\\`-/|
+                                ` -.-' \\ )- ` ( , o o)
+                                       `-     \\`_`"'-
+                            """);
+        } else{
+            System.out.println("""
+                            ("`-''-/").___..--''"`-._\s
+                             `6_ 6  )   `-.  (     ).`-.__.`)\s
+                             (_Y_.)'  ._   )  `._ `. ``-..-'\s
+                               _..`--'_..-_/  /--'_.'
+                              ((((.-''  ((((.'  (((.-'\s
+                            """);
+        }
+    }
+
+    private static void consoleClear() {
+        System.out.println("""
+            \n
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            ~~~~~~~~~~~~//~~~~~~~~~~~~~~~~~~~||~~~~~~~~~~~~~~~~~~~//~~~~~~~~~~~~
+            """);
+    }
+
+    private static void robotSelection(Scanner sc) {
+
+        System.out.println("""
+                ~~~~~~~~~~~~//~~~~~~~~~~~~~~~~~~~||~~~~~~~~~~~~~~~~~~~//~~~~~~~~~~~~
+                """);
+
+        System.out.println("Wähle einen Katzenroboter aus!\n");
+        System.out.println("""
+                     _._      _,-'"" `- ._              ("`-''-/").___..--''"`-._\s
+                    (,-. `._ ,'(         |\\`-/|         `6_ 6  )   `-.  (     ).`-.__.`)\s
+                         `-. -' \\ )-`  (  o o)          (_Y_.)'  ._   )  `._ `. ``-..-'\s
+                               `-    \\`_ `"'-             _..`--'_..-_/  /--'_.'
+                                  --     --                ((((.-''  ((((.'  (((.-'\s
+                                   [ 1 ]                            [ 2 ]
+                """);
+
+
+        int input;
+        do {
+            input = sc.nextInt();
+
+            if (input == 1){
+                robotNumber = 0;
+            } else if (input == 2){
+                robotNumber = 1;
+            }
+        } while (input < 1 || input > 2);
+
+
+        System.out.println("""
+                ~~~~~~~~~~~~//~~~~~~~~~~~~~~~~~~~||~~~~~~~~~~~~~~~~~~~//~~~~~~~~~~~~
+                """);
+
+        System.out.println("""
+                      Sehr schön, jetzt brauchen wir nur noch einen Namen...:
+                """);
+
+        Scanner sc2 = new Scanner(System.in);
+        robotName = sc2.nextLine();
+
+    }
+
+    private static void welcomeScreen(Scanner sc) {
         System.out.println("""
                        \s
                             ⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀
@@ -22,8 +148,8 @@ public class Main {
                             ⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀
                             ⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀
                             ⠀⠀⠀⠘⣿⣿⣿⣿⠟⠁⠀⠀⠀⠹⣿⣿⣿⣿⣿⠟⠁⠀⠀⠹⣿⣿⡿⠀⠀⠀⠀⠀
-                            ⠀⠀⠀⠀⣿⣿⣿⡇⠀⠀⠀⢼⣿⠀⢿⣿⣿⣿⣿⠀⣾⣷⠀⠀⢿⣿⣷⠀⠀⠀⠀⠀
-                            ⠀⠀⠀⢠⣿⣿⣿⣷⡀⠀⠀⠈⠋⢀⣿⣿⣿⣿⣿⡀⠙⠋⠀⢀⣾⣿⣿⠀⠀⠀⠀⠀
+                            ⠀⠀⠀⠀⣿⣿⣿⡇⠀⠀⠀⢼⣿⠀⢿⣿⣿⣿⣿⠀⣾⣷⠀⠀ ⢿⣿⣷⠀⠀⠀⠀⠀
+                            ⠀⠀⠀⢠⣿⣿⣿⣷⡀⠀⠀⠈⠋⢀⣿⣿⣿⣿⣿⡀⠙⠋⠀⢀⣾⣿⣿⡀
                             ⢀⣀⣀⣀⣿⣿⣿⣿⣿⣶⣶⣶⣶⣿⣿⣿⣿⣾⣿⣷⣦⣤⣴⣿⣿⣿⣿⣤⠤⢤⣤⡄
                             ⠈⠉⠉⢉⣙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⣀⣀⣀⡀⠀
                \s
@@ -41,166 +167,24 @@ public class Main {
                 """);
 
         //-----------------------------------
-        input = sc.nextLine();
+        sc.nextLine();
         //-----------------------------------
+    }
 
-        System.out.println("""
-                ~~~~~~~~~~~~//~~~~~~~~~~~~~~~~~~~||~~~~~~~~~~~~~~~~~~~//~~~~~~~~~~~~
-                """);
+    //I might need those in the future
+    private static void generateCoordsX(){
+        char[] coords = {'/', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O'};
 
-        System.out.println("Wähle einen Katzenroboter aus!\n");
-        System.out.println("""
-                     _._      _,-'"" `- ._              ("`-''-/").___..--''"`-._\s
-                    (,-. `._ ,'(         |\\`-/|         `6_ 6  )   `-.  (     ).`-.__.`)\s
-                         `-. -' \\ )-`  (  o o)          (_Y_.)'  ._   )  `._ `. ``-..-'\s
-                               `-    \\`_ `"'-             _..`--'_..-_/  /--'_.'
-                                  --     --                ((((.-''  ((((.'  (((.-'\s
-                                   [ 1 ]                            [ 2 ]
-                """);
+        for (char c : coords){
+            System.out.print("["+c+"]");
 
-        //-----------------------------------
-
-        while (roboNumber < 1){
-            roboNumber = sc.nextInt();
-            while (roboNumber > 2){
-                roboNumber = 0;
-            }
-        }
-
-        //-----------------------------------
-        System.out.println("""
-                ~~~~~~~~~~~~//~~~~~~~~~~~~~~~~~~~||~~~~~~~~~~~~~~~~~~~//~~~~~~~~~~~~
-                """);
-
-        System.out.println("""
-                      Sehr schön, jetzt brauchen wir nur noch einen Namen...:
-                """);
-
-        //-----------------------------------
-        Scanner sc2 = new Scanner(System.in);
-        roboName = sc2.nextLine();
-        //-----------------------------------
-
-
-        int length = 15;
-        int height = 10;
-
-        int currentPosLength = 1;
-        int currentPosHeight = 0;
-
-        boolean gameOn = true;
-        int movementDir = 0;
-
-        while (gameOn) {
-            System.out.println("""
-                \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n
-                ~~~~~~~~~~~~//~~~~~~~~~~~~~~~~~~~||~~~~~~~~~~~~~~~~~~~//~~~~~~~~~~~~
-                """);
-
-            System.out.println("Dein Name: " + username + "\nDein Robotername: " + roboName);
-            int tmp = roboNumber;
-            while (!robotShown) {
-                while (tmp == 1) {
-                    System.out.println("""
-                             _._      _,-'" "`-._
-                            (,-.` ._,'(        |\\`-/|
-                                ` -.-' \\ )- ` ( , o o)
-                                       `-     \\`_`"'-
-                            """);
-                    tmp = 0;
-                }
-
-                while (tmp == 2) {
-                    System.out.println("""
-                            ("`-''-/").___..--''"`-._\s
-                             `6_ 6  )   `-.  (     ).`-.__.`)\s
-                             (_Y_.)'  ._   )  `._ `. ``-..-'\s
-                               _..`--'_..-_/  /--'_.'
-                              ((((.-''  ((((.'  (((.-'\s
-                            """);
-                    tmp = 0;
-                }
-                robotShown = true;
+            if (c == 'O'){
                 System.out.println();
-            }
-            robotShown = false;
-
-            while (currentPosLength == 15){
-                currentPosLength--;
-            }
-
-            while (currentPosLength == 0){
-                currentPosLength++;
-            }
-
-            while (currentPosHeight == 10){
-                currentPosHeight--;
-            }
-
-            while (currentPosHeight == -1){
-                currentPosHeight++;
-            }
-
-            System.out.println("Spielfeld: ");
-            int currentHeight = 0;
-            while (currentHeight < height) {
-                int currentLength = 1;
-                while (currentLength < length) {
-
-                    int tmp2 = currentLength;
-                    int tmp4 = -1;
-                    while (tmp2 == currentPosLength){
-                        int tmp3 = currentHeight;
-                        while (tmp3 == currentPosHeight){
-                            System.out.print("["+roboName.charAt(0)+"]");
-                            tmp4 = tmp2;
-                            tmp2--;
-                            tmp3--;
-                        }
-                        tmp2 = -1;
-                    }
-                    while (currentLength != tmp4){
-                        System.out.print("[ ]");
-                        tmp4 = currentLength;
-                    }
-                    currentLength++;
-                }
-                System.out.println();
-                currentHeight++;
-            }
-
-            System.out.println("""
-                   
-                    Nach Links: [1]
-                    Nach Rechts: [2]
-                    Nach Oben: [3]
-                    Nach Unten: [4]
-                   
-                   """);
-
-            movementDir = sc.nextInt();
-
-            while (movementDir > 0){
-                while (movementDir == 1){
-                    currentPosLength--;
-                    movementDir = 0;
-                }
-                while (movementDir == 2){
-                    currentPosLength++;
-                    movementDir = 0;
-                }
-                while (movementDir == 3){
-                    currentPosHeight--;
-                    movementDir = 0;
-                }
-                while (movementDir == 4){
-                    currentPosHeight++;
-                    movementDir = 0;
-                }
-                while (movementDir > 4){
-                    movementDir = 0;
-                }
             }
         }
     }
+
+    private static void generateCoordsY(int currCord){
+        System.out.print("["+currCord+"]");
+        }
 }
